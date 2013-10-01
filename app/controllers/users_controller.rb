14dc8720554cc6474c9754739a5f28f2092c9ba5
Fred Hudson <remotezen@bell.net>
2013-10-01 13:04:00 -0400

    added users/index.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  def index
+    @user = User.all
+  end
   def create
     @user = User.new(user_params)
     if @user.save
@@ -51,7 +54,7 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
   def signed_in_user
-    unless signed?
+    unless signed_in?
       store_location
       redirect_to signin_url, notice: 
       "Please sign in." unless signed_in?
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,24 @@ require 'spec_helper'
 describe "UserPages" do
 
   subject {page}
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit users_path
+    end
+    it "should list each user" do
+      it { should have_title('All users') }
+      it { should have_content('All users') }
+    end
+    it "should list each user" do
+      User.all.each do |u|
+        expect(page).to have_selector('li', user.name)
+     end
+    end
+  end
+
   describe "signup page" do
     before {visit signup_path}
     it { should have_selector('h3', text: 'Sign up') }
